be074d3e6c276cdaac707c24208fc4b40ff23d20
dev-jino <wlsgh2029@naver.com>
2023-09-01 15:49:28 +0900

    java 16일차 - 자료구조 v1.0

--- src/stackNQueue/bridgeSolution.java
+++ src/stackNQueue/bridgeSolution.java
@@ -7,36 +7,44 @@ public class bridgeSolution {
   public static void main(String[] args) {
     int bridge_length = 2;
     int weight = 10;
-    int[] cars = {7, 4, 5, 6};
+    int[] truck_weights = {7, 4, 5, 6};
 
     int time = 0;
     int weightSum = 0;
     Queue<Integer> q = new LinkedList<>();
 
-    for (int car : cars) {
+    for (int truck_weight : truck_weights) {
       while (true) {
+        System.out.println("truck_weight : " + truck_weight);
+        System.out.println(q);
         if (q.isEmpty()) {
-          q.add(car);
-          weightSum += car;
+          q.add(truck_weight);
+          weightSum += truck_weight;
           time++;
           break;
         } else if (q.size() == bridge_length) {
-          weightSum -= q.poll();
+          int a = q.poll();
+          if (a != 0) {
+            time++;
+          }
+          weightSum -= a;
+//          if (q.peek() != 0) {
+//            time++;
+//          }
+//          weightSum -= q.poll();
         } else {
-          if (weightSum + car > weight) {
+          if (weightSum + truck_weight > weight) {
             q.add(0);
             time++;
-            break;
           } else {
-            q.add(car);
-            weightSum += car;
+            q.add(truck_weight);
+            weightSum += truck_weight;
             time++;
             break;
           }
         }
       }
     }
-
     System.out.println(time);
   }
 }
